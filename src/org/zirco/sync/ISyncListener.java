89feaf72b37f3648f1e4a85861990fc96e458e82
unknown <>
2016-01-16 17:08:40 +0800

    init

---
+++ src/org/zirco/sync/ISyncListener.java
@@ -0,0 +1,11 @@
+package org.zirco.sync;
+//Download by http://www.codefans.net
+public interface ISyncListener {
+	
+	void onSyncProgress(int step, int done, int total);
+	
+	void onSyncEnd(Throwable result);
+	
+	void onSyncCancelled();
+
+}
